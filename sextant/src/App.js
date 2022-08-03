bbcab25adbe93bb457b8513f0df960b4c650da73
l33t-c0d3r-66 <aliraza.arain.12530@gmail.com>
2022-08-03 19:05:50 +0500

    Completed Module 3 Task

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 
 import Banner from './components/Banner/Banner';
 import Exhibit from './components/Exhibits/Exhibits';
+import IPDetails from './components/IPDetails/IPDetails';
 
 function App() {
   return (
     <div className="App">
       <Banner bannerText="SEXTANT" />
-      <Exhibit name="This is an Exhibit 1 Heading">This is Exhibit Content</Exhibit>
+      <Exhibit name="User's IPv4 Address">
+        <IPDetails url='https://api.ipify.org?format=json' />
+      </Exhibit>
+      <Exhibit name="User's IPv6 Address">
+        <IPDetails url='https://api64.ipify.org?format=json' />
+      </Exhibit>
+
     </div>
 
   );
--- sextant/src/components/IPDetails/IPDetails.js
+++ sextant/src/components/IPDetails/IPDetails.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const IPDetails = () => {
+const IPDetails = (props) => {
+    const [ipAddress, setIPAddress] = useState("");
+
+    useEffect(function () {
+        fetch(props.url)
+        .then(response => response.json())
+        .then(data => setIPAddress(data.ip));
+    }, []);
+
     return (
-        <div>
-            
-        </div>
+        <span>
+            {ipAddress}
+        </span>
     );
 }
+
+export default IPDetails;
